c167a64da7943449973fabd44a5bcb9f695d3263
b6m <65096390+b6m@users.noreply.github.com>
2022-05-06 16:40:24 +0200

    added logging, and just made the code better

--- vra.py
+++ vra.py
@@ -1,17 +1,18 @@
 # vra.py
 # author: b6m
 # date: 2022-05-05
-import asyncio
 import httpx
-from datetime import datetime
-from pystyle import Colors, Colorate
+import asyncio
 import random
+import logging
 
-headers          =    {'Authorization': str(input(Colorate.Horizontal(Colors.blue_to_purple,'[?] Enter Your Token : ')))}
-voice_id         =    int(input(Colorate.Horizontal(Colors.blue_to_purple,"[?] Enter A Voice ID : ")))
-amount           =    int(input(Colorate.Horizontal(Colors.blue_to_purple,"[?] How Many Times Do You Want To Change The Region : ")))
+logging.basicConfig(level=logging.INFO,
+                    format='\u001b[36;1m[\u001b[0m%(asctime)s\u001b[36;1m]\u001b[0m %(message)s\u001b[0m',
+                    datefmt='%H:%M:%S'
+                    )
 
-
+headers          =    {'Authorization': str(input('\u001b[36;1m[\u001b[0m?\u001b[36;1m] \u001b[0m Enter Your Token   • '))}
+voice_id         =    int(input('\u001b[36;1m[\u001b[0m?\u001b[36;1m] \u001b[0m Voice ID   •  '))
 
 class VoiceRegionAbuse:
     
@@ -19,27 +20,25 @@ class VoiceRegionAbuse:
         self.group            =    f"https://discord.com/api/v9/channels/{voice_id}/call"
         self.voice_regions    =    ['brazil','hongkong','india','japan','rotterdam','russia','singapore','southafrica','sydney','us-central','us-east','us-south','us-west']
 
+    async def RegionAbuse(self): 
+        while True:
+            try:
 
-    async def RegionAbuse(self):
-        try:
-            for _ in range(amount):
                 async with httpx.AsyncClient() as client:
                     region = random.choice(self.voice_regions)
                     r = await client.patch(self.group, headers=headers, json={"region": region})
+                    
                     if r.status_code in [200, 201, 202, 203, 204, 205, 206, 207, 208, 226]:
-                        _time_ = datetime.now().strftime("%H:%M:%S")
-                        print(Colorate.Horizontal(Colors.white_to_blue, f"[ - @ {_time_} ] Sucessfully Changed To {region}", 1))
+                        logging.info(f'   Successfully Changed Voice Region • {region}')
+                    
                     elif r.status_code == 429:
-                        _time_ = datetime.now().strftime("%H:%M:%S")
-                        print(Colorate.Horizontal(Colors.white_to_red, f"[ - @ {_time_} ] Rate Limited", 1))
+                        logging.warning(f"   Too Many Requests")
+                        
                     elif r.status_code == 404:
-                        _time_ = datetime.now().strftime("%H:%M:%S")
-                        print(Colorate.Horizontal(Colors.white_to_red, f"[ - @ {_time_} ] Voice Call Does Not Exist", 1))
+                        logging.error(f"   Voice Channel Not Found")
+            except Exception as e:
+                logging.critical(f"   {e}")
 
-
-        except Exception as e:
-            _time_ = datetime.now().strftime("%H:%M:%S")
-            print(Colorate.Horizontal(Colors.white_to_red, f"[ - @ {_time_} ] {e}", 1))
 
 async def __run__():
     tasks = []
@@ -47,6 +46,5 @@ async def __run__():
         tasks.append(VoiceRegionAbuse().RegionAbuse())
     await asyncio.gather(*tasks)
     
-        
 if __name__ == "__main__":
     asyncio.run(__run__())
